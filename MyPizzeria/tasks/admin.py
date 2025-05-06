7a8bcdcf76e61dbe81430194229e5ccef39215e9
Ana Laura Chenoweth Galaz <analaura@Anas-MacBook-Air.local>
2025-05-06 12:34:28 -0700

    arreglando errorsitos

--- MyPizzeria/tasks/admin.py
+++ MyPizzeria/tasks/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Tasks
+from .models import InventoryItem
 from .models import TaskProfile
 
 # Register your models here.
-admin.site.register(Tasks)
+admin.site.register(InventoryItem)
 admin.site.register(TaskProfile)
